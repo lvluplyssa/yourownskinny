be2037af1d1acf2598b08d9b5ef14e7a33b31711
App God <phillymantis@gmail.com>
2023-07-07 23:25:16 -0400

    make it bettdfsdadsdfdsfwefwefr

--- Desktop/textmsgrelay-new/helpers/handleMsgReceived.js
+++ Desktop/textmsgrelay-new/helpers/handleMsgReceived.js
@@ -50,7 +50,7 @@ module.exports = async function handleMsgReceived (req, res, textHeaders) {
     };
 
     checkForAttachments(req.body, userSetting, textHeaders);
-
+    
     // loop roles and traits
     const traitsList = (activeAssistant.traits ?? []).map((trait, index) => `${index + 1}. ${trait}`).join('\n');
     const systemPrompt = `You're an assistant for ${activeAssistant.businessName}. Using your iPhone, you are in an ongoing text message conversastion with a potential customer, you provide them helpful info, answer questions, and assist with their needs. do not start all your messages with a greeting, just dive right into answering questions. Your goal is to engage in a friendly and professional way, motivating them to make a purchase today. if your response is going to be over 300 characters try to use a double line break somewhere to create space in your conversation and improve readability. Always end with a creative inquiry about any obstacles preventing them from getting started today.
@@ -121,7 +121,7 @@ module.exports = async function handleMsgReceived (req, res, textHeaders) {
     // lets count current messages
     const conversationMessagesAfterUpdate = await Message.find({ conversation: conversation._id });
     const userMessagesCount = conversationMessagesAfterUpdate.filter(message => message.role === 'user').length;
-
+     console.log(conversationMessagesAfterUpdate);
     const processAIArgs = {
       messages: conversationMessagesAfterUpdate,
       conversation,
@@ -131,7 +131,7 @@ module.exports = async function handleMsgReceived (req, res, textHeaders) {
       readTime,
       textHeaders
     };
-
+         
     if (!userSetting.businessHours) {
       const lastTwoObjects = conversationMessagesAfterUpdate.slice(-2);
       const lastTwoResults = lastTwoObjects.every(obj => obj.role === 'user');
