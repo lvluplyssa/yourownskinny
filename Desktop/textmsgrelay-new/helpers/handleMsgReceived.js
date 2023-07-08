aaa8e79418a7cb1c511b7123c03292e1340a9423
App God <phillymantis@gmail.com>
2023-07-07 23:07:30 -0400

    make it bettdfsdadsdfddd

--- Desktop/textmsgrelay-new/helpers/handleMsgReceived.js
+++ Desktop/textmsgrelay-new/helpers/handleMsgReceived.js
@@ -53,7 +53,7 @@ module.exports = async function handleMsgReceived (req, res, textHeaders) {
 
     // loop roles and traits
     const traitsList = (activeAssistant.traits ?? []).map((trait, index) => `${index + 1}. ${trait}`).join('\n');
-    const systemPrompt = `You're an assistant for ${activeAssistant.businessName}. Using your iPhone, you text potential customers, providing helpful info, answering questions, and assisting with their needs. Only greet a client once in the conversation. Your goal is to engage in a friendly and professional way, motivating them to make a purchase today. if your response is going to be over 300 characters try to use a double line break somewhere to create space in your conversation and improve readability. Always end with a creative inquiry about any obstacles preventing them from getting started today.
+    const systemPrompt = `You're an assistant for ${activeAssistant.businessName}. Using your iPhone, you are in an ongoing text message conversastion with a potential customer, you provide them helpful info, answer questions, and assist with their needs. If you have already greeted this person once in the conversation with a "hey" or "hi" do not do it again, just dive right into answering questions. Your goal is to engage in a friendly and professional way, motivating them to make a purchase today. if your response is going to be over 300 characters try to use a double line break somewhere to create space in your conversation and improve readability. Always end with a creative inquiry about any obstacles preventing them from getting started today.
          
          The following content below details all information about who you are.
    
