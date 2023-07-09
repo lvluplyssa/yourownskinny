ce1677a370960fbc6c87d45be52eda0630b80b28
App God <phillymantis@gmail.com>
2023-07-09 18:28:13 -0400

    make it bettdfsdadsdfdsfwefwsfgssvb dd

---
+++ Desktop/textmsgrelay-new/models/userSetting.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSettingsSchema = new mongoose.Schema({
+  userId: Number,
+  startTime: Number,
+  endTime: Number,
+  activeAssistant: Number,
+  readTime: Number,
+  businessHours: Boolean,
+  assistants: [Number],
+  owners: [{ recipient: String, name: String }],
+  senderName: String,
+  dialCode: String,
+  senderNumber: String,
+  ownerInstructions: [String]
+}, { timestamps: true });
+
+userSettingsSchema.set('toObject', { virtuals: true });
+userSettingsSchema.set('toJSON', { virtuals: true });
+
+userSettingsSchema.index({ senderName: 1 }, { unique: true });
+
+const userSetting = mongoose.model('userSetting', userSettingsSchema);
+
+module.exports = userSetting;
